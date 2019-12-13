d920d541b8337c8fe65c7ba5e06534f849c5a127
Paul Leugemors <pleugemors@yahoo.com>
2019-12-13 20:17:00 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
 class Cat
   # code goes here
+  
+  def initialize(cat_name, owner)
+    @name = cat_name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self 
+  end
+  
+  
+  @@all = []
+  attr_reader :name
+  attr_accessor :owner, :mood
+  
+  def self.all
+    @@all
+  end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
 class Owner
   # code goes here
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self 
+  end
+  
+  @@all = []
+  
+  attr_reader :name, :species
+  
+  def say_species 
+    "I am a #{species}."
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count
+    @@all.size 
+  end
+  
+  def self.reset_all
+    @@all.clear 
+  end
+
 end
